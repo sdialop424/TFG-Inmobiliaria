74153c0fc8bb514939435e59bbf81f5f5205cc3b
sdialop424 <sdialop424@g.educaand.es>
2026-05-20 21:52:52 +0200

    organizacion de css para librerias

--- TFG-Inmobiliaria/vite.config.js
+++ TFG-Inmobiliaria/vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import tailwindcss from '@tailwindcss/vite';
-import react from '@vitejs/plugin-react';
+
 
 
 export default defineConfig({
@@ -10,8 +9,6 @@ export default defineConfig({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
-        tailwindcss(),
-        react(),
     ],
     server: {
         watch: {
